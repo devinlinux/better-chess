5f50612fe42bae5fda7412b75f5824efe7611cb4
devinlinux <ssmith969898@gmail.com>
2023-02-05 23:50:14 -0500

    Started making the StartMenu

---
+++ src/gui/StartMenu.java
@@ -0,0 +1,26 @@
+package gui;
+
+//  imports
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.ImageIcon;
+import javax.swing.Box;
+import javax.imageio.ImageIO;
+
+public class StartMenu implements Runnable {
+    
+    public void run() {
+        
+    }
+}
